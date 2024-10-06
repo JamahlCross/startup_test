767564a75834863dca4783f731504a6d723180b3
JamahlCross <jamahlcross@gmail.com>
2024-10-06 10:19:35 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import App from './App.jsx';
 import React from 'react';
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/startup_test/"}>
         <App />
-    </BrowserRouter>
 );
